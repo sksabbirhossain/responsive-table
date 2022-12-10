f1eb5ea2a4408de9cb7a787e4ac7b5f58c31c8cf
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-12-10 22:01:10 +0600

    fetch userdata and showing in a table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    //fetch data
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setUserData(data));
+  }, []);
   return (
-    <div className="container">
+    <div className="container-fluid">
       <div className="my-4">
         <h2>Data Table</h2>
       </div>
       <div className="table-responsive" id="tableData">
-        <table class="table">
+        <table className="table table-striped table-sm">
           <thead>
             <tr>
               <th scope="col">id</th>
@@ -27,21 +34,28 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            <tr className="table-danger">
-              <td data-title="id">1</td>
-              <td data-title="first_name">Mark</td>
-              <td data-title="last_name">Mark</td>
-              <td data-title="email">sksabbirhossain4@gmail.com</td>
-              <td data-title="gender">Mark</td>
-              <td data-title="ip_address">Mark</td>
-              <td data-title="airport code">Mark</td>
-              <td data-title="time">Mark</td>
-              <td data-title="status">Mark</td>
-              <td data-title="mobile">Mark</td>
-              <td data-title="area">Mark</td>
-              <td data-title="show">Mark</td>
-              <td data-title="edit">Mark</td>
-            </tr>
+            {userData.map((data) => (
+              <tr
+                className={
+                  data.status === "true" ? "table-success" : "table-danger"
+                }
+                key={data.id}
+              >
+                <td data-title="id">{data.id}</td>
+                <td data-title="first_name">{data.first_name}</td>
+                <td data-title="last_name">{data.last_name}</td>
+                <td data-title="email">{data.email}</td>
+                <td data-title="gender">{data.gender}</td>
+                <td data-title="ip_address">{data.ip_address}</td>
+                <td data-title="airport code">{data.airport_code}</td>
+                <td data-title="time">{data.time}</td>
+                <td data-title="status">{data.status}</td>
+                <td data-title="mobile">{data.mobile}</td>
+                <td data-title="area">{data.area}</td>
+                <td data-title="show">{data.show}</td>
+                <td data-title="edit">{data.edit}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
